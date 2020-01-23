e3c5c3fdb5533936976fc148f9249d86ac2bdd99
Matt Bellamy <60126870+mabellamy@users.noreply.github.com>
2020-01-23 09:52:56 -0800

    Create ufo_functions.hpp
    header for ufo.cpp

---
+++ ufo_functions.hpp
@@ -0,0 +1,6 @@
+#include <vector>
+
+void display_misses(int misses);
+void display_status(std::vector<char> incorrect, std::string answer);
+void greet();
+void end_game(std::string answer, std::string codeword);
